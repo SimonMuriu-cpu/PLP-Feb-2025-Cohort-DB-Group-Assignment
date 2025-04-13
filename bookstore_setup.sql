2243a7f0836df3e57fb38dc8daea85b3f42664a3
Lawrenc-e <lawrencejembe4@gmail.com>
2025-04-13 19:45:07 +0300

    bookstore_setup.sql

--- bookstore_setup.sql
+++ bookstore_setup.sql
@@ -1,14 +1,1 @@
-CREATE DATABASE bookstore_db;
 
-
-CREATE USER 'admin_lawrence'@'localhost' IDENTIFIED BY 'Lawrence123!';
-CREATE USER 'admin_simon'@'localhost' IDENTIFIED BY 'Simon123!';
-CREATE USER 'admin_lukhanyo'@'localhost' IDENTIFIED BY 'lukhanyo123!';
-
-
-GRANT ALL PRIVILEGES ON bookstore_db.* TO 'admin_lawrence'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON bookstore_db.* TO 'admin_simon'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON bookstore_db.* TO 'admin_lukhanyo'@'localhost' WITH GRANT OPTION;
-
-
-FLUSH PRIVILEGES;
